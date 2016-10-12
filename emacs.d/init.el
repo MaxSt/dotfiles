daa0dd39a5400393de03e569be84617c5609e429
Max <max@stoerchle.at>
2016-10-12 14:04:50 +0200

    emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,11 @@
+;;; init.el --- Configuration of Emacs
+;;
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(require 'org)
+(require 'ob-tangle)
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(org-babel-load-file (expand-file-name "config.org" init-dir))
