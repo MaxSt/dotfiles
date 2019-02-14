25a701d73f9762710f718fdfb6b573c3c4721d8e
MaxSt <max@stoerchle.at>
2019-02-14 19:07:13 +0100

    Emacs changes
    Emacs changes

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,8 +1,15 @@
-;;; early-init.el --evaluated before init.el
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold 268435456)
+
+;; Disable package.el at startup, since use-package will handle the package loading
+(setq package-enable-at-startup nil)
 
 ;; Prevent the glimpse of un-styled Emacs by setting these early.
 (add-to-list 'default-frame-alist '(tool-bar-lines . 0))
 (add-to-list 'default-frame-alist '(menu-bar-lines . 0))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars))
-
-;;(setq package-quickstart t)
