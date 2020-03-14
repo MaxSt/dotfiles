75d6186dc8c78c41e4ebb29020cbf5bb92c0409c
MaxSt <max@stoerchle.at>
2020-03-14 17:49:35 +0100

    Bootstrap straight.el before requiring org

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,6 +6,25 @@
 
 ;;; Code:
 
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; load org
+(straight-use-package 'org-plus-contrib)
+(straight-use-package 'org)
+
+
 ;; Don't attempt to find/apply special file handlers to files loaded during startup.
 (let ((file-name-handler-alist nil))
   ;; If config is pre-compiled, then load that
